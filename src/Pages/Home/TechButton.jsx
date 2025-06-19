66c0d2fdcf679771548f4231bc2042cf27a402b2
Risan <borhansiddque19@gmail.com>
2025-06-19 21:49:26 +0600

    Update TechButton component's image alt attributes and replace icons in Technologies section

--- src/Pages/Home/TechButton.jsx
+++ src/Pages/Home/TechButton.jsx
@@ -5,7 +5,7 @@ const TechButton = ({icon, title, link}) => {
   return (
    <Link to={link} className=''>
     <button className='btn backdrop-blur-xs bg-black/10 transition-all duration-300 flex items-center px-4 py-2 rounded-lg border border-purple-50 shadow-none hover:shadow-lg hover:shadow-white/20'>
-      <img src={icon} alt="" className='h-8' />
+      <img src={icon} alt={title} className='h-6' />
       <span className='ml-2 text-base font-semibold'>{title}</span>
     </button>
    </Link>
--- src/Pages/Home/Technologies.jsx
+++ src/Pages/Home/Technologies.jsx
@@ -20,8 +20,9 @@ const Technologies = () => {
       <div className="mt-8 flex flex-wrap gap-5">
         <TechButton icon={"https://img.icons8.com/?size=256&id=20909&format=png"} title={"HTML"} link={"https://www.w3schools.com/html/"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=7gdY5qNXaKC0&format=png"} title={"CSS"} link={"https://www.w3schools.com/css/"} />
-        <TechButton icon={"https://img.icons8.com/?size=256&id=4PiNHtUJVbLs&format=png"} title={"Tailwind CSS"} link={"https://tailwindcss.com/"} />
-        <TechButton icon={"https://img.icons8.com/?size=256&id=108784&format=png"} title={"JavaScript"} link={"https://learnjavascript.online/"} />
+        <TechButton icon={"https://i.ibb.co.com/Xk2BNVRn/download.png"} title={"Tailwind CSS"} link={"https://tailwindcss.com/"} />
+        <TechButton icon={"https://i.ibb.co.com/qF7yJwSd/download.png"} title={"JavaScript"} link={"https://learnjavascript.online/"} />
+        <TechButton icon={"https://i.ibb.co.com/c9jTvmt/download.png"} title={"TypwScript"} link={"https://learnjavascript.online/"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=NfbyHexzVEDk&format=png"} title={"React Js"} link={"https://react.dev/"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=62452&format=png"} title={"Firebase"} link={"https://firebase.google.com/"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=54087&format=png"} title={"Node JS"} link={"https://nodejs.org/en"} />
