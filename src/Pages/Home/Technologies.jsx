d9aced3e92db0b550eba52991272674628e5903f
Risan <borhansiddque19@gmail.com>
2025-06-19 22:17:25 +0600

    Add Figma, Github, NPM, and Vercel TechButton components; update TypeScript, Node JS, and MongoDB icons

--- src/Pages/Home/Technologies.jsx
+++ src/Pages/Home/Technologies.jsx
@@ -21,13 +21,17 @@ const Technologies = () => {
         <TechButton icon={"https://img.icons8.com/?size=256&id=20909&format=png"} title={"HTML"} link={"https://www.w3schools.com/html/"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=7gdY5qNXaKC0&format=png"} title={"CSS"} link={"https://www.w3schools.com/css/"} />
         <TechButton icon={"https://i.ibb.co.com/Xk2BNVRn/download.png"} title={"Tailwind CSS"} link={"https://tailwindcss.com/"} />
+        <TechButton icon={"https://i.ibb.co.com/1fKcg32b/download.png"} title={"Figma"} link={"https://www.figma.com/"} />
         <TechButton icon={"https://i.ibb.co.com/qF7yJwSd/download.png"} title={"JavaScript"} link={"https://learnjavascript.online/"} />
-        <TechButton icon={"https://i.ibb.co.com/c9jTvmt/download.png"} title={"TypwScript"} link={"https://learnjavascript.online/"} />
+        <TechButton icon={"https://i.ibb.co.com/c9jTvmt/download.png"} title={"TypwScript"} link={"https://www.typescriptlang.org/"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=NfbyHexzVEDk&format=png"} title={"React Js"} link={"https://react.dev/"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=62452&format=png"} title={"Firebase"} link={"https://firebase.google.com/"} />
-        <TechButton icon={"https://img.icons8.com/?size=256&id=54087&format=png"} title={"Node JS"} link={"https://nodejs.org/en"} />
+        <TechButton icon={"https://i.ibb.co.com/d1Py8nX/download.png"} title={"Node JS"} link={"https://nodejs.org/en"} />
         <TechButton icon={"https://img.icons8.com/?size=256&id=WNoJgbzDr3i2&format=png"} title={"Express JS"} link={"https://expressjs.com/"} />
-        <TechButton icon={"https://img.icons8.com/?size=256&id=74402&format=png"} title={"MongoDB"} link={"https://www.mongodb.com/"} />
+        <TechButton icon={"https://i.ibb.co.com/gbGCy9VW/download.png"} title={"MongoDB"} link={"https://www.mongodb.com/"} />
+        <TechButton icon={"https://i.ibb.co.com/xK941xrQ/download.png"} title={"Github"} link={"https://github.com/"} />
+        <TechButton icon={"https://i.ibb.co.com/Kc0GMSBs/download.png"} title={"NPM"} link={"https://www.npmjs.com/"} />
+        <TechButton icon={"https://i.ibb.co.com/PGXmVr4N/download.png"} title={"Vercel"} link={"https://vercel.com/"} />
         <TechButton icon={"https://img.icons8.com/?size=80&id=EPbEfEa7o8CB&format=png"} title={"PostMan"} link={"https://www.postman.com/"} />
       </div>
     </div>
