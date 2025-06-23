99506c5dfc5171d942b9eafc699c42a7af72c44a
Risan <borhansiddque19@gmail.com>
2025-06-24 00:17:15 +0600

    Update background opacity in GithubCalendar and MyStudioSection components for improved visibility

--- src/Components/GithubCalendar.jsx
+++ src/Components/GithubCalendar.jsx
@@ -3,7 +3,7 @@ import GitHubCalendar from "react-github-calendar";
 
 const GithubCalendar = () => {
   return (
-    <div className="bg-orange-800/20 backdrop-blur-xs w-fit mx-auto py-5 px-10 rounded-2xl">
+    <div className="bg-orange-950/20 backdrop-blur-xs w-fit mx-auto py-5 px-10 rounded-2xl">
       <h1 className="text-4xl text-white  mb-5 text-center">
         Contribution Graph
       </h1>
