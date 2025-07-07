26b346a9742bd88cc58a78bac2bb26717285cebd
Risan <borhansiddque19@gmail.com>
2025-07-07 19:41:56 +0600

    Update button text from "Download CV" to "Download Resume" for clarity

--- src/Pages/Home/AboutSection/MyStudioSection.jsx
+++ src/Pages/Home/AboutSection/MyStudioSection.jsx
@@ -15,7 +15,7 @@ const MyStudioSection = () => {
           download
           className="inline-block rounded-full px-4 py-2 text-base font-bold bg-orange-700 hover:bg-orange-600 text-orange-200 shadow-none hover:shadow-md hover:shadow-orange-500 hover:scale-95 transition-all duration-300"
         >
-          Download CV
+          Download Resume
         </a>
       </div>
 
